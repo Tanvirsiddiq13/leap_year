21f9a10c9573c16978e686cf282564117e0a9e13
Tanvir <tanvirsiddiq96@gmail.com>
2025-02-17 10:07:49 +0000

    convert all lowercase letters to uppercase letters and uppercase to lowercase

--- ex-7.py
+++ ex-7.py
@@ -1,2 +1,9 @@
 user_input = str(input("enter the world you wanna revarse:"))
 print(user_input.swapcase())
+# or another way to solve it
+def sawp_case():
+    return s.swapcase()
+if __name__ = "__main__":
+    s = input()
+    result = sawp_case(s)
+    print(result)
